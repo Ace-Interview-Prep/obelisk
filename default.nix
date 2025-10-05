75bb39e184991ddd72e30989fc7aaa39ccf67d4d
lazyLambda <galen.sprout@gmail.com>
2025-10-05 19:21:48 -0400

    correct attribute of ios options

--- default.nix
+++ default.nix
@@ -355,7 +355,7 @@ in rec {
                 __iosWithConfig = configPath: {
                   ${if self.userSettings.ios == null then null else self.frontendName} = {
                     executableName = "frontend";
-                    "assets" =
+                    "staticSrc" =
                       nixpkgs.obeliskExecutableConfig.platforms.ios.injectMany
                         (self.injectableConfig configPath)
                         (lib.filterAttrs (name: static: static.mobile) self.processedStatic);
