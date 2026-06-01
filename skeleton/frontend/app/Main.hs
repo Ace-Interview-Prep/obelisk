9e38e9b51772d27cfab35291684e5dabb5fdf50d
lazyLambda <galen.sprout@gmail.com>
2026-06-01 17:51:02 -0400

    Fix frontend Main.hs: bridge JSM to IO via jsaddle-warp

--- skeleton/frontend/app/Main.hs
+++ skeleton/frontend/app/Main.hs
@@ -3,12 +3,19 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Data.Proxy (Proxy(..))
-import Jenga.Frontend
+import qualified Data.Map as Map
+import Jenga.Frontend (runFrontend)
 import Common.Route
 import Frontend
 
+#if defined(ghcjs_HOST_OS) || defined(wasm32_HOST_ARCH)
 main :: IO ()
-main = runFrontend (Proxy @FrontendPages) FrontendRoute_Main mempty frontend
+main = runFrontend (Proxy @FrontendPages) FrontendRoute_Main Map.empty frontend
+#else
+import qualified Language.Javascript.JSaddle.Warp as JW
+main :: IO ()
+main = JW.run 3003 $ runFrontend (Proxy @FrontendPages) FrontendRoute_Main Map.empty frontend
+#endif
 
 #if defined(wasm32_HOST_ARCH)
 foreign export javascript "hs_start" main :: IO ()
