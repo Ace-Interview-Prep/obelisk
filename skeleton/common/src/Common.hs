dadaa0faa25b82002f633b8f937cbee4e5e27a4f
lazyLambda <galen.sprout@gmail.com>
2026-06-01 17:15:14 -0400

    Hide conflicting Id export in Common.hs

--- skeleton/common/src/Common.hs
+++ skeleton/common/src/Common.hs
@@ -8,7 +8,7 @@ import Common.Route as Common
 import Common.Types as Common
 
 #if !defined(javascript_HOST_ARCH) && !defined(wasm32_HOST_ARCH)
-import Common.Schema as Common
+import Common.Schema as Common hiding (Id)
 import Common.Request as Common
 import Common.View as Common
 #endif
