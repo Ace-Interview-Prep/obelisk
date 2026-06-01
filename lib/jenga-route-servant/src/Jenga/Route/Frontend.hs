e3b4acb1d7585dc111953c0c386b5f01bfdd8fe2
lazyLambda <galen.sprout@gmail.com>
2026-06-01 13:01:02 -0400

    Fix broken import ordering in Frontend.hs

--- lib/jenga-route-servant/src/Jenga/Route/Frontend.hs
+++ lib/jenga-route-servant/src/Jenga/Route/Frontend.hs
@@ -42,13 +42,12 @@ import           Control.Monad (when)
 
 import           Reflex (Dynamic, Event, Reflex)
 import qualified Reflex as R
-import           Reflex.Dom.Core
 import           Data.Default (Default(def))
+import           Reflex.Dom.Core
                    ( GhcjsDomSpace, EventResult, ElementConfig, AttributeName(..)
                    , elementConfig_initialAttributes, elementConfig_eventSpec
                    , addEventSpecFlags, preventDefault
                    , domEvent, EventName(Click)
-                   
                    )
 
 import           Reflex.Effectful.Types (KnownTimeline)
