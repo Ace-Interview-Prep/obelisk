b50920ed88a6406e5e035e953a33a146086e477a
lazyLambda <galen.sprout@gmail.com>
2026-06-01 15:28:49 -0400

    Hide more Reflex.Dom.Core re-exports that conflict with Reflex.Effectful

--- lib/jenga-route-servant/src/Jenga/Frontend.hs
+++ lib/jenga-route-servant/src/Jenga/Frontend.hs
@@ -52,7 +52,7 @@ import           System.Info (os)
 
 import           Effectful (Eff, IOE, (:>))
 
-import           Reflex.Dom.Core hiding (blank)
+import           Reflex.Dom.Core hiding (blank, elAttr, el, text, dynText, display, elClass, elAttr')
 import           GHCJS.DOM.Types (JSM)
 import qualified GHCJS.DOM as DOM
 import qualified GHCJS.DOM.Document as DOM
